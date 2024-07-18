6eab055d1d525984f816bfd80477220a80a0a39f
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-18 19:05:12 +0530

    chore: set component type dynamically - option 1

--- react-essentials/src/components/Examples.jsx
+++ react-essentials/src/components/Examples.jsx
@@ -28,6 +28,7 @@ export default function Examples() {
   return (
     <Section id="examples" title="Examples">
       <Tabs
+        buttonsContainer="menu"
         buttons={
           <>
             <TabButton
--- react-essentials/src/components/Tabs.jsx
+++ react-essentials/src/components/Tabs.jsx
@@ -1,7 +1,9 @@
-export default function Tabs({ children, buttons }) {
+export default function Tabs({ children, buttons, buttonsContainer }) {
+  const ButtonsContainer = buttonsContainer;    // jsx element, not inbuilt element, therefore should start with capital letter
+
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
       {children}
     </>
   );
