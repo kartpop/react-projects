c006977a8add568a498e7e285b34ae4656c5a55e
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-18 14:41:58 +0530

    chore: derive and output data based on state

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import TabButton from "./components/TabButton";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please select a topic');
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton)
+    setSelectedTopic(selectedButton);
     console.log(selectedTopic); // will print the previous value of selectedTopic, as React schedules state updates AFTER the component has reloaded
   }
 
@@ -33,12 +33,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          <p>{selectedTopic}</p>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
