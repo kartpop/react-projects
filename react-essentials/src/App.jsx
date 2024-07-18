c582b827fda5886ebea7926b69c5e8a49578a227
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-18 07:23:37 +0530

    chore: use props to dynamically populate data in components and make them reusable

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -1,6 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -21,11 +22,32 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building blocks."
+              image={componentsImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
       <Header />
     </div>
