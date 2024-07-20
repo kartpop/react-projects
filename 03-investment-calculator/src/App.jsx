ae6821ab5d4cdd6d6dfdcb0c80f4b3dab84eeb09
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-20 17:15:27 +0530

    feat: initial commit for investment calculator

---
+++ 03-investment-calculator/src/App.jsx
@@ -0,0 +1,15 @@
+import Result from "./components/Result";
+import UserInput from "./components/UserInput";
+
+function App() {
+  return (
+    <main>
+      <div id="user-input">
+        <UserInput></UserInput>
+        <Result></Result>
+      </div>
+    </main>
+  );
+}
+
+export default App;
