bba824485ceaf020acd26e13a44f332a66b3c2e0
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-20 18:54:00 +0530

    feat: add user input with two way binding

--- 03-investment-calculator/src/App.jsx
+++ 03-investment-calculator/src/App.jsx
@@ -1,13 +1,66 @@
+import { useState } from "react";
 import Result from "./components/Result";
 import UserInput from "./components/UserInput";
 
 function App() {
+  const [initialInvestment, setInitialInvestment] = useState(15000);
+  const [annualInvestment, setAnnualInvestment] = useState(900);
+  const [expectedReturn, setExpectedReturn] = useState(5.5);
+  const [duration, setDuration] = useState(12);
+
+  function handleInitialInvestmentChange(event) {
+    setInitialInvestment(parseInt(event.target.value));
+    console.log(event.target.value);
+  }
+
+  function handleAnnualInvestmentChange(event) {
+    setAnnualInvestment(parseInt(event.target.value));
+    console.log(event.target.value);
+  }
+
+  function handleExpectedReturnChange(event) {
+    setExpectedReturn(parseFloat(event.target.value));
+    console.log(event.target.value);
+  }
+
+  function handleDurationChange(event) {
+    setDuration(parseInt(event.target.value));
+    console.log(event.target.value);
+  }
+
   return (
     <main>
       <div id="user-input">
-        <UserInput></UserInput>
-        <Result></Result>
+        <ol className="input-group">
+          <UserInput
+            label="Initial Investment"
+            value={initialInvestment}
+            step="1000"
+            onInputChanged={handleInitialInvestmentChange}
+          ></UserInput>
+          <UserInput
+            label="Annual Investment"
+            value={annualInvestment}
+            step="1000"
+            onInputChanged={handleAnnualInvestmentChange}
+          ></UserInput>
+        </ol>
+        <ol className="input-group">
+          <UserInput
+            label="Expected Return"
+            value={expectedReturn}
+            step="0.1"
+            onInputChanged={handleExpectedReturnChange}
+          ></UserInput>
+          <UserInput
+            label="Duration"
+            value={duration}
+            step="1000"
+            onInputChanged={handleDurationChange}
+          ></UserInput>
+        </ol>
       </div>
+      <Result></Result>
     </main>
   );
 }
--- 03-investment-calculator/src/components/UserInput.jsx
+++ 03-investment-calculator/src/components/UserInput.jsx
@@ -1,3 +1,14 @@
-export default function UserInput() {
-    return <></>
+export default function UserInput({ label, value, step, onInputChanged }) {
+  return (
+    <div>
+      <label className="label">{label}</label>
+      <input
+        className="input"
+        type="number"
+        value={value}
+        step={step}
+        onChange={onInputChanged}
+      />
+    </div>
+  );
 }
