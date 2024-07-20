d4d07a60bbe61b9b19fdc90ef8b6a3d0b4384e13
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-20 19:02:46 +0530

    chore: wip for results table

--- 03-investment-calculator/src/components/Result.jsx
+++ 03-investment-calculator/src/components/Result.jsx
@@ -1,3 +1,25 @@
 export default function Result() {
-    return <></>
+  return (
+    <div id="result">
+      <table>
+        <thead>
+          <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+            <th>Column 4</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+            <td>Data 3</td>
+            <td>Data 4</td>
+          </tr>
+          {/* Add more <tr>...</tr> here for more data rows */}
+        </tbody>
+      </table>
+    </div>
+  );
 }
