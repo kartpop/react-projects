d87bcb710005ebaab904bc7a403ead6b6fbf8836
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-22 17:42:41 +0530

    feat: complete label and input migration to tailwind

--- 04-styling-react-apps/src/components/Input.jsx
+++ 04-styling-react-apps/src/components/Input.jsx
@@ -1,16 +1,23 @@
 export default function CustomInput({ label, invalid, ...props }) {
   let labelClasses =
-    "block mb-2 text-xs font-bold tracking-wide uppercase text-stone-300";
+    "block mb-2 text-xs font-bold tracking-wide uppercase";
+    let inputClasses = "w-full px-3 py-2 leading-tight border rounded shadow";
 
   if (invalid) {
-    labelClasses =
-      "block mb-2 text-xs font-bold tracking-wide uppercase text-red-500";
+    labelClasses +=
+      " text-red-400";
+      inputClasses += "text-red-500 bg-red-300 border-red-300";
+  } else {
+    labelClasses +=
+      " text-stone-300";
+      inputClasses += " text-gray-700 bg-stone-300";
   }
+
   return (
     <p>
       <label className={labelClasses}>{label}</label>
       <input
-        className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow bg-stone-300"
+        className={inputClasses}
         {...props}
       />
     </p>
