51e5c8c410e34dae60f1b6bfd4a4dd331972b403
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-23 06:26:03 +0530

    chore: manage set player name using multiple states - inefficient

--- 05-timer-challenge/src/components/Player.jsx
+++ 05-timer-challenge/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [name, setName] = useState("unknown entity");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleNameChange(event) {
+    setSubmitted(false);
+    setName(event.target.value);
+  }
+
+  function handleSetNameButton() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? name : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input onChange={handleNameChange} type="text" value={name}/>
+        <button onClick={handleSetNameButton}>Set Name</button>
       </p>
     </section>
   );
