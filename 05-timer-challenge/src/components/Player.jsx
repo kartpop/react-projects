a7f28183d1c1f9899b0749758849e42406f32978
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-23 06:46:58 +0530

    feat: manage set player name using ref - efficient

--- 05-timer-challenge/src/components/Player.jsx
+++ 05-timer-challenge/src/components/Player.jsx
@@ -1,24 +1,19 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
-  const [name, setName] = useState("unknown entity");
-  const [submitted, setSubmitted] = useState(false);
+  const [enteredName, setEnteredName] = useState("unknown entity");
+  const name = useRef();  // gives access to the jsx element and all its props where this ref is set
 
-  function handleNameChange(event) {
-    setSubmitted(false);
-    setName(event.target.value);
-  }
-
-  function handleSetNameButton() {
-    setSubmitted(true);
+  function handleClick() {
+    setEnteredName(name.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? name : 'unknown entity'}</h2>
+      <h2>Welcome {enteredName ?? "unknown entity"}</h2>
       <p>
-        <input onChange={handleNameChange} type="text" value={name}/>
-        <button onClick={handleSetNameButton}>Set Name</button>
+        <input ref={name} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
