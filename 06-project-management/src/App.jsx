faab5badcda16d170c5396d517bae8cc925a08fa
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-24 12:57:06 +0530

    feat: save new projects and display list of projects

--- 06-project-management/src/App.jsx
+++ 06-project-management/src/App.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
 import CreateProject from "./components/CreateProject";
 
 function App() {
+  const [newProjectActive, setNewProjectActive] = useState(false);
+  const [projects, setProjects] = useState([]);
+
+  const handleAddProjectCick = () => {
+    setNewProjectActive(true);
+  };
+
+  const handleNewProjectCreated = ({ title, description, dueDate }) => {
+    setProjects([...projects, { title, description, dueDate }]);
+    setNewProjectActive(false);
+  };
+
   return (
     <>
       <div className="flex">
@@ -10,13 +23,27 @@ function App() {
           </h2>
           <div className="flex">
             <div className="p-2"></div>
-            <button className="py-2 px-4 bg-slate-500 text-white">
+            <button
+              onClick={handleAddProjectCick}
+              className="py-2 px-4 bg-slate-500 text-white"
+            >
               + Add Project
             </button>
           </div>
+          <div className="p-4">
+            {projects.map((project, index) => (
+              <div key={index} className="border border-slate-500 p-2 m-2">
+                <h3 className="text-white text-left font-bold text-lg">
+                  {project.title}
+                </h3>
+              </div>
+            ))}
+          </div>
         </div>
         <div className="w-3/4 bg-white h-screen p-4">
-          <CreateProject></CreateProject>
+          {newProjectActive && (
+            <CreateProject onSave={handleNewProjectCreated}></CreateProject>
+          )}
         </div>
       </div>
     </>
--- 06-project-management/src/components/CreateProject.jsx
+++ 06-project-management/src/components/CreateProject.jsx
@@ -1,15 +1,54 @@
+import { useState } from "react";
+
 import LabelInput from "./LabelInput";
 
-export default function CreateProject() {
+export default function CreateProject({ onSave }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  function handleTitleChange(e) {
+    setTitle(e.target.value);
+  }
+
+  function handleDescriptionChange(e) {
+    setDescription(e.target.value);
+  }
+
+  function handleDateChange(e) {
+    setDueDate(e.target.value);
+  }
+
+  function handleSave() {
+    onSave({ title, description, dueDate });
+  }
+
   return (
     <>
       <div className="flex justify-end m-2">
         <button className="m-2">Cancel</button>
-        <button className="m-2">Save</button>
+        <button className="m-2" onClick={handleSave}>
+          Save
+        </button>
       </div>
-      <LabelInput label="Title" type="text" />
-      <LabelInput label="Description" type="text" />
-      <LabelInput label="Due Date" type="date" />
+      <LabelInput
+        label="Title"
+        type="text"
+        value={title}
+        onChange={handleTitleChange}
+      />
+      <LabelInput
+        label="Description"
+        type="text"
+        value={description}
+        onChange={handleDescriptionChange}
+      />
+      <LabelInput
+        label="Due Date"
+        type="date"
+        value={dueDate}
+        onChange={handleDateChange}
+      />
     </>
   );
 }
