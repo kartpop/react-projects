83293e30ffa7f8ee5cec74d8cb7cd69eebffb3d7
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-24 08:00:16 +0530

    chore: initial commit for project management, add basic structure in App

---
+++ 06-project-management/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div className="flex">
+        <div className="w-1/4 bg-slate-800 h-screen">
+          <h2 className="text-white text-left font-bold text-xl p-4">Your Projects</h2>
+        </div>
+        <div className="w-3/4 bg-white h-screen ">
+          {/* Element for displaying the selected project */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
