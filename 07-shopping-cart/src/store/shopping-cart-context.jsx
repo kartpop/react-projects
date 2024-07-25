b45fdc4381d47d3503ee954da0f48e7038e1935b
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-25 21:13:33 +0530

    feat: use context to both read and edit state across components

--- 07-shopping-cart/src/store/shopping-cart-context.jsx
+++ 07-shopping-cart/src/store/shopping-cart-context.jsx
@@ -2,5 +2,6 @@ import { createContext } from "react";
 
 export const CartContext = createContext({
     items: [], // not required, but helpful for intellisense
+    addItemsToCart: () => {}, // not required, but helpful for intellisense
 });
 
