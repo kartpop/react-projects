ec4aedaf3f04da6d0fb6396941dfdd809e4d032c
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-27 17:56:22 +0530

    feat: highlight selected answer and provide feedback to user - buggy
    - selected answer jumps around when highlighted as selected and when providing feedback whether answer is correct or wrong
    - possibly because we are shuffling answer options on every render

--- 09-quiz-app/src/components/Quiz.jsx
+++ 09-quiz-app/src/components/Quiz.jsx
@@ -5,8 +5,9 @@ import QuizTimer from "./QuizTimer";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
+  const [answerState, setAnswerState] = useState('');
 
-  const qIdx = userAnswers.length;
+  const qIdx = answerState === '' ? userAnswers.length : userAnswers.length - 1;
 
   if (qIdx === QUESTIONS.length) {
     return (
@@ -17,8 +18,25 @@ export default function Quiz() {
     );
   }
 
+  // Handle answerState in order to give feedback to the user:
+  // 1. Set answerState to 'answered' when the user clicks an answer - yellow
+  // 2. Set answerState to 'correct' or 'wrong' after 1 second - green or red
+  // 3. Reset answerState to '' after 2 seconds - so that qIdx is set to next question
   const handleAnswerClick = useCallback(function handleAnswerClick(answer) {
+    setAnswerState('answered');
     setUserAnswers((prevAnswers) => [...prevAnswers, answer]);
+
+    setTimeout(() => {
+      if (answer === QUESTIONS[qIdx].answers[0]) {
+        setAnswerState('correct');
+      } else {
+        setAnswerState('wrong');
+      }
+
+      setTimeout(() => {
+        setAnswerState('');
+      }, 2000);
+    }, 1000);
   }, []);
 
   const handleSkipAnswer = useCallback(
@@ -40,11 +58,24 @@ export default function Quiz() {
         <h2>{QUESTIONS[qIdx].text}</h2>
       </div>
       <div id="answers">
-        {shuffledAnswers.map((answer) => (
-          <li key={answer} className="answer">
-            <button onClick={() => handleAnswerClick(answer)}>{answer}</button>
-          </li>
-        ))}
+        {shuffledAnswers.map((answer) => {
+          const isSelected = userAnswers[userAnswers.length - 1] === answer;
+          let cl = '';
+
+          if (isSelected && answerState === 'answered') {
+            cl = 'selected';
+          } 
+
+          if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+            cl = answerState;
+          }
+
+          return (<li key={answer} className="answer">
+            <button className={cl} onClick={() => handleAnswerClick(answer)}>{answer}</button>
+          </li>)
+        }
+          
+        )}
       </div>
     </div>
   );
