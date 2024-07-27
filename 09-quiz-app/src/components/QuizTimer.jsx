7271ee714460a134cf2bb598a5dc41e71d3c5708
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-27 16:45:36 +0530

    fix: clearInterval and clearTimer
    - the progress bar depeltes in 5s instead of 10s because strict mode causes components to render twice in dev environment, therefore there is a pause in between the progress bar depleting and the next question appearing
    
    - clearInterval and clearTimer should be done anyway for hygiene

--- 09-quiz-app/src/components/QuizTimer.jsx
+++ 09-quiz-app/src/components/QuizTimer.jsx
@@ -4,15 +4,23 @@ export default function QuizTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => { // without useEffect, setInterval would be called on every render after setRemainingTime is called causing multiple timers to run
-    setInterval(() => {
+    const interval = setInterval(() => {
       setRemainingTime((prevTime) => prevTime - 100);
     }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   useEffect(() => { // we don't want timeout to be set on every render when remainitTime is updated
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       onTimeout();
     }, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [onTimeout, timeout]);
 
   return <progress id="progress" max={timeout} value={remainingTime}></progress>;
