120960a43b36fcc0a83a70f7d2eba68edf8cb256
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-27 20:01:57 +0530

    fix: useRef for shuffledAnswers and move quiz completed condition after all hooks
    - useRef for shuffledAnswers ensures that order of options remains constant through the display of a single question
    - jsx return of quiz completed was at incorrect location; it was occuring before few hooks, which violates the rule of hooks that they must be called unconditionally

--- 09-quiz-app/src/components/Quiz.jsx
+++ 09-quiz-app/src/components/Quiz.jsx
@@ -1,13 +1,49 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef } from "react";
 import QUESTIONS from "../questions";
 import quizcomplete from "../assets/quiz-complete.png";
 import QuizTimer from "./QuizTimer";
 
 export default function Quiz() {
-  const [userAnswers, setUserAnswers] = useState([]);
-  const [answerState, setAnswerState] = useState('');
+  const shuffledAnswers = useRef();
 
-  const qIdx = answerState === '' ? userAnswers.length : userAnswers.length - 1;
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [answerState, setAnswerState] = useState("");
+
+  const qIdx = answerState === "" ? userAnswers.length : userAnswers.length - 1;
+
+  // Handle answerState in order to give feedback to the user:
+  // 1. Set answerState to 'answered' when the user clicks an answer - yellow
+  // 2. Set answerState to 'correct' or 'wrong' after 1 second - green or red
+  // 3. Reset answerState to '' after 2 seconds - so that qIdx is set to next question
+  const handleAnswerClick = useCallback(function handleAnswerClick(answer) {
+    setAnswerState("answered");
+    setUserAnswers((prevAnswers) => [...prevAnswers, answer]);
+
+    setTimeout(() => {
+      if (answer === QUESTIONS[qIdx].answers[0]) {
+        setAnswerState("correct");
+      } else {
+        setAnswerState("wrong");
+      }
+
+      setTimeout(() => {
+        setAnswerState("");
+      }, 2000);
+    }, 1000);
+  }, []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleAnswerClick(null),
+    [handleAnswerClick]
+  );
+
+  if (!shuffledAnswers.current && qIdx < QUESTIONS.length) {
+    {
+      shuffledAnswers.current = [...QUESTIONS[qIdx].answers].sort(
+        () => Math.random() - 0.5
+      );
+    }
+  }
 
   if (qIdx === QUESTIONS.length) {
     return (
@@ -18,35 +54,6 @@ export default function Quiz() {
     );
   }
 
-  // Handle answerState in order to give feedback to the user:
-  // 1. Set answerState to 'answered' when the user clicks an answer - yellow
-  // 2. Set answerState to 'correct' or 'wrong' after 1 second - green or red
-  // 3. Reset answerState to '' after 2 seconds - so that qIdx is set to next question
-  const handleAnswerClick = useCallback(function handleAnswerClick(answer) {
-    setAnswerState('answered');
-    setUserAnswers((prevAnswers) => [...prevAnswers, answer]);
-
-    setTimeout(() => {
-      if (answer === QUESTIONS[qIdx].answers[0]) {
-        setAnswerState('correct');
-      } else {
-        setAnswerState('wrong');
-      }
-
-      setTimeout(() => {
-        setAnswerState('');
-      }, 2000);
-    }, 1000);
-  }, []);
-
-  const handleSkipAnswer = useCallback(
-    () => handleAnswerClick(null),
-    [handleAnswerClick]
-  );
-
-  const shuffledAnswers = [...QUESTIONS[qIdx].answers].sort(
-    () => Math.random() - 0.5
-  );
   return (
     <div id="quiz">
       <div id="question">
@@ -58,24 +65,29 @@ export default function Quiz() {
         <h2>{QUESTIONS[qIdx].text}</h2>
       </div>
       <div id="answers">
-        {shuffledAnswers.map((answer) => {
+        {shuffledAnswers.current.map((answer) => {
           const isSelected = userAnswers[userAnswers.length - 1] === answer;
-          let cl = '';
+          let cl = "";
 
-          if (isSelected && answerState === 'answered') {
-            cl = 'selected';
-          } 
+          if (isSelected && answerState === "answered") {
+            cl = "selected";
+          }
 
-          if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+          if (
+            (answerState === "correct" || answerState === "wrong") &&
+            isSelected
+          ) {
             cl = answerState;
           }
 
-          return (<li key={answer} className="answer">
-            <button className={cl} onClick={() => handleAnswerClick(answer)}>{answer}</button>
-          </li>)
-        }
-          
-        )}
+          return (
+            <li key={answer} className="answer">
+              <button className={cl} onClick={() => handleAnswerClick(answer)}>
+                {answer}
+              </button>
+            </li>
+          );
+        })}
       </div>
     </div>
   );
