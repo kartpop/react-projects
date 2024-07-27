4f1403615711cc73c72e0add8c0927db6aac91be
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-27 07:03:21 +0530

    chore: shuffle answers and show quiz complete

--- 09-quiz-app/src/components/quiz.jsx
+++ 09-quiz-app/src/components/quiz.jsx
@@ -1,22 +1,35 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizcomplete from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const qIdx = userAnswers.length;
 
+  if (qIdx === QUESTIONS.length) {
+    return (
+      <div id="summary">
+        <img src={quizcomplete} alt="Trophy!" />
+        <h2>Quiz completed!</h2>
+      </div>
+    );
+  }
+
   function handleAnswerClick(answer) {
     setUserAnswers((prevAnswers) => [...prevAnswers, answer]);
   }
 
+  const shuffledAnswers = [...QUESTIONS[qIdx].answers].sort(
+    () => Math.random() - 0.5
+  );
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[qIdx].text}</h2>
       </div>
       <div id="answers">
-        {QUESTIONS[qIdx].answers.map((answer) => (
+        {shuffledAnswers.map((answer) => (
           <li key={answer} className="answer">
             <button onClick={() => handleAnswerClick(answer)}>{answer}</button>
           </li>
