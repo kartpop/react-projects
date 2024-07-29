b980705f1f8d86e58869a0d8f063e438316c5c1c
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-30 05:12:22 +0530

    chore: wip, get places from backend using http request with fetch

--- 11-place-picker-http-db/src/components/AvailablePlaces.jsx
+++ 11-place-picker-http-db/src/components/AvailablePlaces.jsx
@@ -1,6 +1,15 @@
-import Places from './Places.jsx';
+import Places from "./Places.jsx";
+import { useEffect, useState } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => { // avoid infinite loop: --> component --> fetch --> state change --> component...
+    fetch("http://localhost:3000/places")
+      .then((response) => response.json())
+      .then((data) => setAvailablePlaces(data.places));
+  }, []);
+
   return (
     <Places
       title="Available Places"
