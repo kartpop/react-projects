ddf7e215d3cb2139b4dc1a96d1e19385c9e295ff
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-30 18:17:53 +0530

    chore: better validation, but still showing error message too early

--- 12-react-forms/src/components/StateLogin.jsx
+++ 12-react-forms/src/components/StateLogin.jsx
@@ -18,7 +18,7 @@ export default function Login() {
     }));
   }
 
-  const invalidEmail = !(formInputs.email.includes("@")) 
+  const invalidEmail = formInputs.email != "" && !(formInputs.email.includes("@")) 
 
   return (
     <form onSubmit={handleSubmit}>
