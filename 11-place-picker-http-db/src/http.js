435bee677edfe652a1d004fed499a4981d2d4a3d
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-30 11:04:31 +0530

    feat: PUT request to /places endpoint

--- 11-place-picker-http-db/src/http.js
+++ 11-place-picker-http-db/src/http.js
@@ -1,10 +1,28 @@
 export async function fetchAvailablePlaces() {
-    const response = await fetch("http://localhost:3000/places");
-    const data = await response.json();
+  const response = await fetch("http://localhost:3000/places");
+  const data = await response.json();
 
-    if (!response.ok) {
-      throw new Error("Could not fetch places.");
-    }
+  if (!response.ok) {
+    throw new Error("Could not fetch places.");
+  }
 
-    return data.places;
+  return data.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ places }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Could not update user places.");
+  }
+
+  const resData = await response.json();
+
+  return resData.message;
 }
