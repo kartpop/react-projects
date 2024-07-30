bf001ad98522064d34cb0a8dabd496b5d557fb42
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-30 06:26:08 +0530

    feat: add error handling to http request

--- 11-place-picker-http-db/src/components/AvailablePlaces.jsx
+++ 11-place-picker-http-db/src/components/AvailablePlaces.jsx
@@ -1,22 +1,41 @@
 import Places from "./Places.jsx";
+import ErrorComponent from "./ErrorComponent.jsx";
 import { useEffect, useState } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
-  useEffect(() => { // avoid infinite loop: --> component --> fetch --> state change --> component...
+  useEffect(() => {
+    // avoid infinite loop: --> component --> fetch --> state change --> component...
     async function fetchPlaces() {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const data = await response.json();
-      setAvailablePlaces(data.places);
+      try {
+        setIsFetching(true);
+        const response = await fetch("http://localhost:3000/placesss");
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Could not fetch places.");
+        }
+
+        setAvailablePlaces(data.places);
+      } catch (error) {
+        setError(error);
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
 
+  if (error) {
+    return (
+      <ErrorComponent title="Error" message={error.message} />
+    );
+  }
+  
   return (
     <Places
       title="Available Places"
