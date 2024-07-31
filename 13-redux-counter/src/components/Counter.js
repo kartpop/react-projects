5eb9407498a81298ed23c6d301c7c2715ef71f15
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-07-31 12:29:47 +0530

    feat: use action payload with dispatch

--- 13-redux-counter/src/components/Counter.js
+++ 13-redux-counter/src/components/Counter.js
@@ -7,6 +7,7 @@ const Counter = () => {
 
   const incrementHandler = () => { dispatch({ type: 'increment' }); };
   const decrementHandler = () => { dispatch({ type: 'decrement' }); };
+  const increaseHandler = () => { dispatch({ type: 'increase', amount: 5 }); };
 
   const toggleCounterHandler = () => {};
 
@@ -16,6 +17,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
