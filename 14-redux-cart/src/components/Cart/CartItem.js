e8c61d9c08149138661ddd9ce1093ecebed7ea00
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-01 07:43:24 +0530

    feat: hookup +/- buttons in cart

--- 14-redux-cart/src/components/Cart/CartItem.js
+++ 14-redux-cart/src/components/Cart/CartItem.js
@@ -1,14 +1,25 @@
-import classes from './CartItem.module.css';
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { id, title, quantity, total, price } = props.item;
+
+  const dispatch = useDispatch();
+
+  const increaseItemQuantityHandler = () => {
+    dispatch(cartActions.addItem({ id, title, price }));
+  };
+  const decreaseItemQuantityHandler = () => {
+    dispatch(cartActions.removeItem(id));
+  };
 
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${total.toFixed(2)}{" "}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +28,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={decreaseItemQuantityHandler}>-</button>
+          <button onClick={increaseItemQuantityHandler}>+</button>
         </div>
       </div>
     </li>
