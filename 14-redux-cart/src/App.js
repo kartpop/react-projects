9c3ff3b498d3082c85dd5ac51595ea54cdfbec89
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-01 13:51:02 +0530

    feat: add notification for http request

--- 14-redux-cart/src/App.js
+++ 14-redux-cart/src/App.js
@@ -3,26 +3,71 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    fetch(
-      "https://react-projects-7ec5a-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-      {
-        method: "PUT",
-        body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-projects-7ec5a-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
       }
-    );
-  }, [cart]);
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    sendCartData().catch(() => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        ></Notification>
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
