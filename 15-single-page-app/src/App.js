8a5d851df039ced71f430275a55fa31e97443dfc
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-01 20:08:48 +0530

    feat: navigate between pages with links

--- 15-single-page-app/src/App.js
+++ 15-single-page-app/src/App.js
@@ -2,7 +2,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }, { path: "/products", element: <ProductsPage /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
--- 15-single-page-app/src/pages/Home.js
+++ 15-single-page-app/src/pages/Home.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <p>
+        Go to <Link to="/products">Products</Link>
+      </p>
     </div>
   );
 }
