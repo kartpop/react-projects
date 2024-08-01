7e39f0fe8af935b7152d92ef73ebd77238f25613
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-01 20:28:55 +0530

    feat: wrap pages with navigation root layout

---
+++ 15-single-page-app/src/components/MainNavigation.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function MainNavigation() {
+  return (
+    <header>
+      <div>My App</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
