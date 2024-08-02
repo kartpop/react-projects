c946c60ab223f320514cc65bb90c68ffb53e9d1d
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-02 18:17:25 +0530

    chore: move loader logic back to Events, refer to func pointer in browser router

--- 16-events-single-page-app/frontend/src/pages/Events.js
+++ 16-events-single-page-app/frontend/src/pages/Events.js
@@ -1,15 +1,25 @@
-
-import { useLoaderData } from 'react-router-dom';
-import EventsList from '../components/EventsList';
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-    const events = useLoaderData();
+  const events = useLoaderData();
 
-    return (
+  return (
     <>
-       <EventsList events={events} />
+      <EventsList events={events} />
     </>
   );
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // TODO
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
