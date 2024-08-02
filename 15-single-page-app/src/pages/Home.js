71e798e46532b9a2741276c542a966f87266f58b
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-02 06:16:11 +0530

    feat: navigate programmatically, say on an event; in example, a Link element is better suited

--- 15-single-page-app/src/pages/Home.js
+++ 15-single-page-app/src/pages/Home.js
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/products");
+  }
+
   return (
     <div>
       <h1>Home Page</h1>
       <p>
         Go to <Link to="/products">Products</Link>
+      </p>
+      <p>
+        <button onClick={handleClick}>Go to Products</button>
       </p>
     </div>
   );
