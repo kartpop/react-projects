4d33b3143d6433b268dc40d635035b66a2384907
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-02 16:28:18 +0530

    feat: add dummy events with links

--- 16-events-single-page-app/frontend/src/pages/Events.js
+++ 16-events-single-page-app/frontend/src/pages/Events.js
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Programming for everyone",
+    description: "Learn the basics of programming",
+  },
+  {
+    id: "e2",
+    title: "Database fundamentals",
+    description: "Learn the basics of databases",
+  },
+];
+
 function EventsPage() {
-    return <h1>Events</h1>
+  return (
+    <>
+      <h1>Events</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={`${event.id}`}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default EventsPage;
