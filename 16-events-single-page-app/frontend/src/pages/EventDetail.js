86db88af0738ca3e33afbcc0b6346743c9360aca
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-03 07:20:53 +0530

    chore: load event detail page

--- 16-events-single-page-app/frontend/src/pages/EventDetail.js
+++ 16-events-single-page-app/frontend/src/pages/EventDetail.js
@@ -1,15 +1,26 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-    const params = useParams();
+  const data = useLoaderData();
 
-    return (
-      <>
-        <h1>EventDetailPage</h1>
-        <p>Event ID: {params.eventId}</p>
-      </>
-    );
-  
+  return (
+    <>
+      <EventItem event={data.event}></EventItem>
+    </>
+  );
 }
 
 export default EventDetailPage;
+
+export async function loader({ params }) {
+  const response = await fetch(
+    `http://localhost:8080/events/${params.eventId}`
+  );
+
+  if (!response.ok) {
+    throw json({ message: "Failed to load event" }, { status: 500 });
+  } else {
+    return response;
+  }
+}
