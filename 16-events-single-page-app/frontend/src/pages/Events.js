de3ee7ddcf3e4e7322356e7e34913ea914115325
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-04 06:25:20 +0530

    feat: show loading message with Suspense and Await components in conjuction with defer with react router

--- 16-events-single-page-app/frontend/src/pages/Events.js
+++ 16-events-single-page-app/frontend/src/pages/Events.js
@@ -1,5 +1,6 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
   const response = useLoaderData();
@@ -11,20 +12,29 @@ function EventsPage() {
   const events = response.events;
 
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function fetchEvent() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     throw json({ message: "Failed to load events" }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export async function loader() {
+  return defer({
+    events: fetchEvent(),
+  });
 }
