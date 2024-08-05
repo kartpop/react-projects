fddbd53de3a82470354bce65f9991e9226e2824a
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-05 16:00:53 +0530

    feat: invalidate cached queries when related data is mutated

--- 17-events-tanstack-query/src/components/Events/NewEvent.jsx
+++ 17-events-tanstack-query/src/components/Events/NewEvent.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { createNewEvent } from "../../util/http.js";
+import { createNewEvent, queryClient } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
@@ -10,7 +10,10 @@ export default function NewEvent() {
 
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
-    onSuccess: () => navigate("/events"),
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['events']});
+      navigate("/events")
+    } 
   });
 
   function handleSubmit(formData) {
