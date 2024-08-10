0f470d12180ddede5a7cdf9780b0bd696b4ee9d9
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-10 20:38:42 +0530

    feat: use query key as query fn input

--- 17-events-tanstack-query/src/components/Events/NewEventsSection.jsx
+++ 17-events-tanstack-query/src/components/Events/NewEventsSection.jsx
@@ -1,26 +1,29 @@
-import { useQuery } from '@tanstack/react-query'
-import { fetchEvents } from '../../util/http.js';
-import LoadingIndicator from '../UI/LoadingIndicator.jsx';
-import ErrorBlock from '../UI/ErrorBlock.jsx';
-import EventItem from './EventItem.jsx';
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvents } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
 
 export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ['events'],
-    queryFn: fetchEvents,
-    staleTime: 5000,  // time in ms after which tanstack query will still yield cached data, but will send a backend request to update
+    queryKey: ["events", { max: 3 }],
+    queryFn: ({ signal, queryKey }) => fetchEvents({ signal, ...queryKey[1] }),
+    staleTime: 5000, // time in ms after which tanstack query will still yield cached data, but will send a backend request to update
     // gcTime: 1000, // time in ms after which data will be garbage collected, so tanstack can only yield the data after backend responds
   });
 
   let content;
 
-  if (isPending ) {
+  if (isPending) {
     content = <LoadingIndicator />;
   }
 
   if (isError) {
     content = (
-      <ErrorBlock title="An error occurred" message={error.info?.message || "Failed to fetch events"} />
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch events"}
+      />
     );
   }
 
