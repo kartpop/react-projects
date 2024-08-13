46c765585332e7be29e48910b748d986fecb45ee
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-13 16:41:32 +0530

    feat: add typed todos

--- 18-react-ts/src/App.tsx
+++ 18-react-ts/src/App.tsx
@@ -1,10 +1,13 @@
 import Todos from './components/Todos'
+import Todo from './models/todo'
 
 function App() {
+  const todos = [new Todo('Learn React'), new Todo('Learn GenAI')]
+
   return (
     <>
       <div>
-        <Todos items={["Learn React", "Learn GenAI"]}></Todos>
+        <Todos items={todos}></Todos>
       </div>
     </>
   )
