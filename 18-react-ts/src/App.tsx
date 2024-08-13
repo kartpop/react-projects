f291fcad66cd5c5245b41eedd076833fb5f8ba7a
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-13 20:30:18 +0530

    feat: use type with state management and function props

--- 18-react-ts/src/App.tsx
+++ 18-react-ts/src/App.tsx
@@ -1,16 +1,26 @@
-import Todos from './components/Todos'
-import Todo from './models/todo'
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo('Learn React'), new Todo('Learn GenAI')]
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAddTodo = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <>
       <div>
+        <NewTodo onAddTodo={handleAddTodo}></NewTodo>
         <Todos items={todos}></Todos>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
