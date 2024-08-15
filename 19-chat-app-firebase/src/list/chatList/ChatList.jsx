375a936cafa94e01edee8f2bdb73d40cff78e017
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-15 07:00:17 +0530

    chore: use webkit-scrollbar, complete chatList styling

--- 19-chat-app-firebase/src/list/chatList/ChatList.jsx
+++ 19-chat-app-firebase/src/list/chatList/ChatList.jsx
@@ -1,15 +1,70 @@
 import "./chatList.css";
+import { useState } from "react";
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
+
   return (
     <div className="chatList">
-      <div className="chatList">
-        <div className="search">
-          <div className="searchBar">
-            <img src="./search.png" alt="search" />
-            <input type="text" placeholder="Search" />
-          </div>
-          <img src="./plus.png" alt="plus" className="add" />
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="search" />
+          <input type="text" placeholder="Search" />
+        </div>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt="plus"
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
         </div>
       </div>
     </div>
