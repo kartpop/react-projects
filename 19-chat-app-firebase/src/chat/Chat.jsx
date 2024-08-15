b87342c9a9c32c931fc3c0af60cd08a07f5e4fb0
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-15 16:23:07 +0530

    feat: setup chat bottom - message input, emmoji etc

--- 19-chat-app-firebase/src/chat/Chat.jsx
+++ 19-chat-app-firebase/src/chat/Chat.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+import EmojiPicker from "emoji-picker-react";
 import "./chat.css";
 
 const Chat = () => {
+  const [openEmojiPicker, setOpenEmojiPicker] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleEmojiSelected = (e) => {
+    setMessage((prev) => prev + e.emoji);
+    setOpenEmojiPicker(false);
+  };
+
   return (
     <div className="chat">
       <div className="top">
@@ -18,7 +28,33 @@ const Chat = () => {
         </div>
       </div>
       <div className="center"></div>
-      <div className="bottom"></div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpenEmojiPicker((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker
+              open={openEmojiPicker}
+              onEmojiClick={handleEmojiSelected}
+            />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
     </div>
   );
 };
