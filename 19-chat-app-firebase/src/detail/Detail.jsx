f4ba1938a3c95292dd19021fae13905d34da2d15
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-15 19:43:13 +0530

    feat: style Detail component

--- 19-chat-app-firebase/src/detail/Detail.jsx
+++ 19-chat-app-firebase/src/detail/Detail.jsx
@@ -1,7 +1,64 @@
 import "./detail.css";
 
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Some description of Jane Doe again</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://elements-resized.envatousercontent.com/envato-shoebox/c36c/ddd4-90a2-4470-b4b2-0775f899b70c/DSC_0662.jpg?w=1400&cf_fit=scale-down&mark-alpha=18&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark4.png&q=85&format=auto&s=e10cc33f749cc24055e856a241ecf69e65d46ef03cc10502f51f7c1c50ed62bb"
+                  alt=""
+                />
+                <span>photo_something.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://elements-resized.envatousercontent.com/envato-shoebox/c36c/ddd4-90a2-4470-b4b2-0775f899b70c/DSC_0662.jpg?w=1400&cf_fit=scale-down&mark-alpha=18&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark4.png&q=85&format=auto&s=e10cc33f749cc24055e856a241ecf69e65d46ef03cc10502f51f7c1c50ed62bb"
+                  alt=""
+                />
+                <span>photo_something.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
